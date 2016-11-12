1b4f384cdacd4fad6f4899092365da9710e38a33
Parker Diamond <Parker@parker-diamonds-macbook.local>
2016-11-12 14:35:55 -0500

    Added some pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
+import { LandingPage } from '../pages/landing/landing';
 
 
 @Component({
   template: `<ion-nav [root]="rootPage"></ion-nav>`
 })
 export class MyApp {
-  rootPage = TabsPage;
+  rootPage = LandingPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { GoalsPage } from '../pages/goals/goals';
 import { ProfilePage } from '../pages/profile/profile';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { RegistrationPage } from '../pages/registration/registration';
+import { LoginPage } from '../pages/login/login';
+import { LandingPage } from '../pages/landing/landing';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { TabsPage } from '../pages/tabs/tabs';
     GoalsPage,
     ProfilePage,
     HomePage,
-    TabsPage
+    TabsPage,
+    RegistrationPage,
+    LoginPage,
+    LandingPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -23,7 +29,10 @@ import { TabsPage } from '../pages/tabs/tabs';
     GoalsPage,
     ProfilePage,
     HomePage,
-    TabsPage
+    TabsPage,
+    RegistrationPage,
+    LoginPage,
+    LandingPage
   ],
   providers: []
 })
