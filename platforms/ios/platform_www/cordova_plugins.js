154bada25578f762b26b8e26840086c7ace07a85
jaredmichaelsmith <jared@jaredsmith.io>
2016-11-12 10:59:52 -0600

    Add platforms config

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -48,6 +48,14 @@ module.exports = [
             "cordova.plugins.Keyboard"
         ],
         "runs": true
+    },
+    {
+        "file": "plugins/com.telerik.plugins.healthkit/www/HealthKit.js",
+        "id": "com.telerik.plugins.healthkit.HealthKit",
+        "pluginId": "com.telerik.plugins.healthkit",
+        "clobbers": [
+            "window.plugins.healthkit"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -58,7 +66,8 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "4.0.0",
     "cordova-plugin-statusbar": "2.2.0",
     "cordova-plugin-whitelist": "1.3.0",
-    "ionic-plugin-keyboard": "2.2.1"
+    "ionic-plugin-keyboard": "2.2.1",
+    "com.telerik.plugins.healthkit": "0.5.1"
 }
 // BOTTOM OF METADATA
 });
