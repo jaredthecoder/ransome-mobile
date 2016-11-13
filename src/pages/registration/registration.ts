0893d4bee75dad4c86d73cf9c678314034cbf1f9
Parker Diamond <Parker@parker-diamonds-macbook.local>
2016-11-12 21:59:42 -0500

    Stuff

--- src/pages/registration/registration.ts
+++ src/pages/registration/registration.ts
@@ -1,5 +1,8 @@
+import {Injectable}     from '@angular/core';
+import {Http, Response, URLSearchParams} from '@angular/http';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import 'rxjs/Rx';
 
 /*
   Generated class for the Registration page.
@@ -13,10 +16,37 @@ import { NavController } from 'ionic-angular';
 })
 export class RegistrationPage {
 
-  constructor(public navCtrl: NavController) {}
+   first_name;
+   last_name;
+   email;
+   address;
+   city;
+   state;
+   zipcode;
+   password;
+
+  constructor(public navCtrl: NavController, public http: Http) {}
 
   ionViewDidLoad() {
     console.log('Hello RegistrationPage Page');
+  }
+
+  register()
+  {
+    let params: URLSearchParams = new URLSearchParams();
+        params.set('first_name', this.first_name);
+        params.set('last_name', this.last_name);
+        params.set('email', this.email); 
+        params.set('address', this.address); 
+        params.set('city', this.city); 
+        params.set('state', this.state); 
+        params.set('zipcode', this.zipcode); 
+        params.set('password', this.password); 
+
+   let url = "http://10.67.48.90:8000/";
+
+   return this.http.post(url, { search: params })
+            .map(res => res.json())
   }
 
 }
